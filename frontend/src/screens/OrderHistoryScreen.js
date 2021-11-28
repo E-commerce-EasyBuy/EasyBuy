a8112f5a22858c266294e65bc04533b0a7cd51fc
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-11-28 13:55:38 +0530

    fronted - cart

---
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listOrderMine } from '../actions/orderActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { deleteOrder, listOrders } from '../actions/orderActions';
+import { ORDER_DELETE_RESET } from '../constants/orderConstants';
+
+export default function OrderHistoryScreen(props) {
+  const orderMineList = useSelector((state) => state.orderMineList);
+  const { loading, error, orders } = orderMineList;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listOrderMine());
+  }, [dispatch]);
+
+  const deleteHandler = (order) => {
+    if (window.confirm('Are you sure to delete?')) {
+      dispatch(deleteOrder(order._id));
+      window.location.reload(false)
+    }
+  };
+  return (
+    <div>
+      <h1>Order History</h1>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>{order.totalPrice.toFixed(2)}</td>
+                <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
+                <td>
+                  {order.isDelivered
+                    ? order.deliveredAt.substring(0, 10)
+                    : 'No'}
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() => {
+                      props.history.push(`/order/${order._id}`);
+                    }}
+                  >
+                    VIEW
+                  </button>
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() => deleteHandler(order)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
